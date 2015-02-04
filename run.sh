0af296f99be22555ca0d486c45045b2582586cc0
NTrevisani <nicolo.trevisani@cern.ch>
2015-02-04 14:24:33 +0100

    adding the hadd of some root files

--- run.sh
+++ run.sh
@@ -39,6 +39,8 @@ WJetsFakes_Total   \
 
 #rm -rf rootfiles/${NJETS}jet
 
+mkdir rootFiles
+
 # Loop
 for CHANNEL in $CHANNELS; do
 
@@ -47,17 +49,17 @@ for CHANNEL in $CHANNELS; do
 	mkdir rootFiles/${NJETS}jets/
 	mkdir rootFiles/${NJETS}jets/${CHANNEL}	
 	root -l -b -q "RunPROOF_test.C($LUMINOSITY,\"$TEST\",\"$SAMPLE\","$NJETS",\"$CHANNEL\")"
-	mv csa15_${SAMPLE}.root rootFiles/${NJETS}jets/${CHANNEL}
+	mv ${SAMPLE}.root rootFiles/${NJETS}jets/${CHANNEL}
   
     done
 
-#    OUTPATH=rootfiles/${NJETS}jet/${CHANNEL}
+    OUTPATH=rootfiles/${NJETS}jets/${CHANNEL}
 
 #    Merge some MC files
-#    hadd ${OUTPATH}/WW.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu.root 
-#    hadd ${OUTPATH}/WW_pow.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu_pow.root 
-#    hadd ${OUTPATH}/WW_mc.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu_mcnlo.root
-#    hadd ${OUTPATH}/Wgamma.root ${OUTPATH}/WgammaNoStar.root ${OUTPATH}/WgammaStar.root ${OUTPATH}/GamGamWW.root 
+    hadd ${OUTPATH}/WW.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu.root 
+    hadd ${OUTPATH}/WW_pow.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu_pow.root 
+    hadd ${OUTPATH}/WW_mc.root     ${OUTPATH}/ggWWto2L.root     ${OUTPATH}/WWTo2L2Nu_mcnlo.root
+    hadd ${OUTPATH}/Wgamma.root ${OUTPATH}/WgammaNoStar.root ${OUTPATH}/WgammaStar.root ${OUTPATH}/GamGamWW.root 
 
 done
 
